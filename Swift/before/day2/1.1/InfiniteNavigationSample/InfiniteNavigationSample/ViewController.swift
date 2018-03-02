f8aacca4fea7c7d46e735bc9da78f5c2571b3e31
Naofumi-Okano <okano-naofumi@cyber-z.co.jp>
2018-03-02 19:33:45 +0900

    NavigactionControllerの保持するStackの階層数をNavigationBarに表示するようにする

--- Swift/before/day2/1.1/InfiniteNavigationSample/InfiniteNavigationSample/ViewController.swift
+++ Swift/before/day2/1.1/InfiniteNavigationSample/InfiniteNavigationSample/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        super.title = navigationController?.viewControllers.count.description
         // Do any additional setup after loading the view, typically from a nib.
     }
 
