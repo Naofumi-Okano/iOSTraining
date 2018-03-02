3daa5b2bfe7dba1c06f776edf6165971f29ef1f7
Naofumi-Okano <okano-naofumi@cyber-z.co.jp>
2018-03-02 12:40:31 +0900

    正解データのオブジェクトの再現

--- Swift/before/day1/1.1/JSON.playground/Contents.swift
+++ Swift/before/day1/1.1/JSON.playground/Contents.swift
@@ -1,3 +1,17 @@
 //: Playground - noun: a place where people can play
 
-let dict: [String : Any] = [:]
+
+struct User {
+    var name: String
+    var gender: Int
+    var era: Int
+    var age: Int
+    var visits: [Int]?
+}
+
+let dict: [String : Any] = ["users":[
+        User(name: "Dr. Emmett Brown", gender:1, era: 1985, age: 65, visits:[1885, 1955, 1985, 2015]),
+        User(name: "Marty McFly", gender:1, era: 1985, age: 17, visits:[1885, 1955, 1985, 2015]),
+        User(name: "Lorraine Baines", gender:0, era: 1955, age: 18, visits:nil),
+    ]
+]
