936bc666b48c75dbca9ef437cfac48889fa93062
Naofumi-Okano <okano-naofumi@cyber-z.co.jp>
2018-03-05 16:07:05 +0900

    TableViewを利用したプロジェクトのコミット

---
+++ Swift/before/day2/3.1/UITableViewSample/UITableViewSample/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  UITableViewSample
+//
+//  Created by 岡野 直史 on 2018/03/05.
+//  Copyright © 2018年 岡野 直史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+
+}
+
