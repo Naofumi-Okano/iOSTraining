21a64dc5d2de48ddcac55ff436cb226b9b3059d0
Naofumi-Okano <okano-naofumi@cyber-z.co.jp>
2018-03-06 13:25:42 +0900

    実際の処理を追加。Buttonを押すとLabelが変わる

--- Swift/before/day3/3.4/PNotificationSample/PNotificationSample/FirstViewController.swift
+++ Swift/before/day3/3.4/PNotificationSample/PNotificationSample/FirstViewController.swift
@@ -21,15 +21,8 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func pushNotificationTapped(_ sender: UIButton) {
+        let dict = ["key" : "value"]
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "pushNotificationTapped"), object: self, userInfo: dict)
     }
-    */
-
 }
--- Swift/before/day3/3.4/PNotificationSample/PNotificationSample/SecondViewController.swift
+++ Swift/before/day3/3.4/PNotificationSample/PNotificationSample/SecondViewController.swift
@@ -9,27 +9,26 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet weak final var label: UILabel!
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(type(of: self).recieveNotification(_:)), name: Notification.Name(rawValue: "pushNotificationTapped"), object: nil)
+    }
+    
+    @objc func recieveNotification(_ notification: Notification) {
+        guard let value = notification.userInfo?["key"] as? String else { return }
+        label.text = value
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
