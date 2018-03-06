0deb4e24447809690942b0dbc0f48f16c118ae1d
Naofumi-Okano <okano-naofumi@cyber-z.co.jp>
2018-03-06 15:41:43 +0900

    ボタン関連の処理を追加

--- Swift/before/day3/4.3/WebViewSample/WebViewSample/ViewController.swift
+++ Swift/before/day3/4.3/WebViewSample/WebViewSample/ViewController.swift
@@ -11,6 +11,17 @@ import WebKit
 
 final class ViewController: UIViewController {
     let webView = WKWebView(frame: .zero, configuration: WKWebViewConfiguration())
+    
+    lazy var backButton: UIBarButtonItem = {
+        return UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(ViewController.backButtonTapped(_:)))
+    }()
+    lazy var forwardButton: UIBarButtonItem = {
+        return UIBarButtonItem(title: "Forward", style: .plain, target: self, action: #selector(ViewController.forwardButtonTapped(_:)))
+    }()
+    lazy var relaodButton: UIBarButtonItem = {
+        return UIBarButtonItem(title: "Relaod", style: .plain, target: self, action: #selector(ViewController.reloadButtonTapped(_:)))
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,11 +39,46 @@ final class ViewController: UIViewController {
         let url = URL(string: "https://github.com/apple")!
         let request = URLRequest(url: url)
         webView.load(request)
+        
+        navigationController?.isToolbarHidden = false
+        backButton.isEnabled = false
+        forwardButton.isEnabled = false
+        toolbarItems = [backButton, forwardButton, relaodButton]
+        
+        [
+            #keyPath(WKWebView.canGoBack),
+            #keyPath(WKWebView.canGoForward),
+            #keyPath(WKWebView.title)
+            ].forEach { keyPath in
+                webView.addObserver(self, forKeyPath: keyPath, options: [.new, .old], context: nil)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        switch (keyPath, change?[.newKey], change?[.oldKey]) {
+        case ((#keyPath(WKWebView.canGoBack))?, let newVal as Bool, let oldVal as Bool) where newVal != oldVal:
+            backButton.isEnabled =  newVal
+            
+        case ((#keyPath(WKWebView.canGoForward))?, let newVal as Bool, let oldVal as Bool) where newVal != oldVal:
+            forwardButton.isEnabled =  newVal
+            
+        case ((#keyPath(WKWebView.title))?, let newVal as String, let oldVal as String) where newVal != oldVal:
+            title = newVal
+            
+        default:
+            break
+        }
+    }
+    @objc func backButtonTapped(_ sender: UIBarButtonItem) {
+        webView.goBack()
+    }
+    
+    @objc func forwardButtonTapped(_ sender: UIBarButtonItem) {
+        webView.goForward()
+    }
+    
+    @objc func reloadButtonTapped(_ sender: UIBarButtonItem) {
+        webView.reload()
     }
 }
 
