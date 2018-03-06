f7d5fdb744c5d94cba8c2bdcfe83d1eedd7c4259
Naofumi-Okano <okano-naofumi@cyber-z.co.jp>
2018-03-06 13:08:17 +0900

    タブのViewに対してUIViewControllerを貼り付ける

---
+++ Swift/before/day3/3.4/PNotificationSample/PNotificationSample/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  PNotificationSample
+//
+//  Created by 岡野 直史 on 2018/03/06.
+//  Copyright © 2018年 岡野 直史. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
