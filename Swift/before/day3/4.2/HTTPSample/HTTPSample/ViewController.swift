de48f42104c960d9bfa10e15eb20d8d085ba74aa
Naofumi-Okano <okano-naofumi@cyber-z.co.jp>
2018-03-06 14:40:43 +0900

    Refactoring

--- Swift/before/day3/4.2/HTTPSample/HTTPSample/ViewController.swift
+++ Swift/before/day3/4.2/HTTPSample/HTTPSample/ViewController.swift
@@ -9,20 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var textView: UITextView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func searchButtonTapped(_ sender: UIButton) {
         guard let username = textField.text else { return }
         fetchUser(username)
